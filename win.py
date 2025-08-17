1bdbc77f23d80eb9bf4f82accdfb98514600cc17
code_ninja <instructor@codeninja.sg>
2025-08-17 11:20:51 +0800

    Add files via upload

---
+++ win.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Buttons & Displays")
+
+title_label = tk.Label(root, text="Hello Tkinter!", font=("Arial", 18))
+title_label.pack(pady=10)
+
+name_label = tk.Label (root, text="your name:")
+name_label.pack()
+
+
+
+root.mainloop()
