f9a034155ea9bcae48b766e7ff35226b0c7fddb6
code_ninja <instructor@codeninja.sg>
2025-08-31 11:34:32 +0800

    Update win.py

--- win.py
+++ win.py
@@ -8,7 +8,11 @@ title_label.pack(pady=10)
 
 name_label = tk.Label (root, text="your name:")
 name_label.pack()
-
-
+name_entry = tk.Entry(root, text= "your name")
+name_entry.pack(pady=5)
+def say_hello():
+    name = name_entry.get().strip()
+    if name:
+        pass
 
 root.mainloop()
