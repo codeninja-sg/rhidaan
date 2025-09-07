0e5d3092deb95e477ec2d4e84775c15fea6247cb
code_ninja <instructor@codeninja.sg>
2025-09-07 11:20:17 +0800

    Update kopi.py

--- kopi.py
+++ kopi.py
@@ -10,4 +10,12 @@ elif choice== '2':
 else:
     print ('not a choice - press 1 or 2.' )
 
+choice = input ('Do you want milk (1) or no milk (2) ')
+if choice == '1':
+        drink = drink
+elif choice == '2':
+     drink = drink + '-o'
+
+else:
+     print ("not a choice - pres 1 or 2.")
 print (' Your order is ' + drink)
