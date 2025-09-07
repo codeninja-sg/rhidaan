9902c5ca9c46140368ecdab38dee179b5c2d7817
code_ninja <instructor@codeninja.sg>
2025-09-07 11:21:55 +0800

    Update win.py

--- win.py
+++ win.py
@@ -13,6 +13,19 @@ name_entry.pack(pady=5)
 def say_hello():
     name = name_entry.get().strip()
     if name:
-        pass
+        title_label.config(text=f"Hello,{name}!")
+    else:
+        title_label.config(text="Hello , Tkinter")
+hello_btn = tk.Button(root,text= "Say Hello", command=say_hello)
+hello_btn.pack(pady=5)
+
+clicks = tk.IntVar(value=0)
+def count_click():
+    clicks.set(clicks.get)()( + 1)
+    counter_label.config(text=f"clicks: {clicks.get}")
+    
+
+
+
 
 root.mainloop()
