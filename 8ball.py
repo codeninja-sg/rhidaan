a34bd8cee4f2002c958338523bf85660a24ad4c0
code_ninja <instructor@codeninja.sg>
2025-11-09 11:33:04 +0800

    Create 8ball.py

---
+++ 8ball.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+import random
+root = tk.Tk()
+root.title("magic 8 ball")
+root .geometry("400x300")
+title_label = tk.Label(root, text= "magic 8 Ball",font=("Arial", 20))
+title_label.pack(pady=10)
+
+question_label = tk.Entry(root, text="Ask a yes/no question")
+question_entry.pack(pady=5)
+
+question_entry = tk.Entry(root, width=30 justify= "center")
+answer.label.pack(pady=20)
+
+answer_label.pack(pady.20 )
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
