f26a9379f2bd0ab961883eef85a4f99addbf8b0f
code_ninja <instructor@codeninja.sg>
2025-11-30 11:17:56 +0800

    Update 8ball.py

--- 8ball.py
+++ 8ball.py
@@ -6,13 +6,39 @@ root .geometry("400x300")
 title_label = tk.Label(root, text= "magic 8 Ball",font=("Arial", 20))
 title_label.pack(pady=10)
 
-question_label = tk.Entry(root, text="Ask a yes/no question")
+question_label = tk.Label(root, text="Ask a yes/no question")
+question_label.pack(pady=5)
+
+question_entry = tk.Entry(root, width=30, justify= "center")
 question_entry.pack(pady=5)
 
-question_entry = tk.Entry(root, width=30 justify= "center")
-answer.label.pack(pady=20)
+answer_label = tk.Label(root, text="", font=("Arial",16))
+answer_label.pack(pady=20)
 
-answer_label.pack(pady.20 )
+answers=[
+"yes!",
+"no!",
+"Maybe...",
+"Ask again later",
+"Definitely!",
+"I don't think so",
+"Try again",
+"Absolutely",
+"Up to you"
+]
+ask_btn = tk.Button(root, text="ask", width=12)
+ask_btn.pack(pady=5)
+
+def give_answer():
+   question = question_entry.get().strip()
+
+#connect the button
+def ask_pressed():
+   give_answer()
+ask_btn.config(command=ask_pressed)
+
+
+
 
 
 
