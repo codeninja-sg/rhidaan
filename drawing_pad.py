79c0c18ffd773aeef2b2fc8d2f30efcbf6860f07
code_ninja <instructor@codeninja.sg>
2026-01-11 11:19:52 +0800

    Create drawing_pad.py

---
+++ drawing_pad.py
@@ -0,0 +1,19 @@
+import tkinter as tk 
+
+root = tk.Tk ()
+root.title("Drawing pad")
+root.geometry("800x600")
+controls = tk.Frame(root)
+controls.grid(row=0, column=0, sticky="ew" , padx=8, pady=8)
+
+canvas = tk.Canvas (root, bg="white" , width=780, height=480)
+canvas.grid(row=1, column=0, sticky="nsew" , padx=8 , pady=8 )
+
+
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+
+
+
+root.mainloop()
