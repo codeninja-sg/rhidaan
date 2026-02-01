597aaeebb9d4566edcceda05b3a22965c2c66bc1
code_ninja <instructor@codeninja.sg>
2026-02-01 11:25:02 +0800

    Update drawing_pad.py

--- drawing_pad.py
+++ drawing_pad.py
@@ -13,6 +13,28 @@ canvas.grid(row=1, column=0, sticky="nsew" , padx=8 , pady=8 )
 root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
+current_color = "Black" 
+pen_size = 5 
+last_x, last_y = None, None
+
+def start_draw(event):
+    global last_x, last_y 
+    last_x, last_y = event.x, event.y
+
+def draw(event):
+    global last_x, last_y
+    if last_x is not None and last_y is not None:
+        canvas.create_line (last_x, last_y, event.x,event.y, fill=current_color, width=pen_size, 
+        capstyle=tk.ROUND)  
+        last_x, last_y = event.x, event.y
+
+def end_draw (event):
+    global last_x, last_y 
+    last_x, last_y = None,None 
+
+canvas.bind("<Button-1>", start_draw)   
+canvas.bind ("< B1- Motion >", draw)   
+canvas.addtag_closest    
 
 
 
