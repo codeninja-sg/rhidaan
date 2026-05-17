e141ce76c86845a0e299e5c5613029d965ac11c3
code_ninja <instructor@codeninja.sg>
2026-05-17 11:25:44 +0800

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,25 @@
+import tkinter as tk 
+import random 
+root = tk.TK()
+root.title ("Snake - 1 ")
+
+SIZE = 20 
+W = 400
+H = 400
+
+canvas = tk.Canvas(root, width=W, height=H bg= "White")
+canvas.pack()
+
+snake = [(10, 10)]
+
+dx=1
+dy=0
+
+food = (random.randint(0, W//SIZE - 1)
+        random.randint(0, H//SIZE - 1))
+
+def draw ():
+    canvas.delete("all")
+
+fx, fy = food 
+canvas.create_rectangle(fx*SIZE, fy*SIZE)
