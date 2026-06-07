69b8c37c2b31ebb4012a1f907b47340f03209b7d
code_ninja <instructor@codeninja.sg>
2026-06-07 11:18:52 +0800

    Update snake.py

--- snake.py
+++ snake.py
@@ -7,10 +7,10 @@ SIZE = 20
 W = 400
 H = 400
 
-canvas = tk.Canvas(root, width=W, height=H bg= "White")
+canvas = tk.Canvas(root, width=W, height=H, bg= "White")
 canvas.pack()
 
-snake = [(10, 10)]
+snake = (10, 10)
 
 dx=1
 dy=0
@@ -21,5 +21,18 @@ food = (random.randint(0, W//SIZE - 1)
 def draw ():
     canvas.delete("all")
 
-fx, fy = food 
-canvas.create_rectangle(fx*SIZE, fy*SIZE)
+    fx, fy = food 
+    canvas.create_rectangle(fx*SIZE, fy*SIZE,
+                            fx*SIZE+SIZE , y*SIZE+SIZE,
+                            fill="green")
+
+
+    for (x , y) in snake:
+        canvas.create_rectangle(x*SIZE, y*SIZE,
+                                 x*SIZE+SIZE, y*SIZE+SIZE,
+                                 fill= "green")
+        
+def game_loop():
+    print()
+
+root.mainloop()
